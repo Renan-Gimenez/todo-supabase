1d9205ecab4ccab0c3c0c6e48ea92599e3ddd23a
Renan Gimenez <renangiimenez@gmail.com>
2023-10-21 11:36:02 -0300

    feat: implement clear all button

---
+++ src/components/ClearAllTasks.tsx
@@ -0,0 +1,26 @@
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+
+interface Props{
+    onClick: () => void,
+}
+
+export default function ClearAllTasks({ onClick }:Props) {
+    const [isLoading, setIsLoading] = useState(false);
+
+    return(
+        <button 
+            className="block mx-auto my-4 bg-red-500 px-4 py-2 text-white rounded-full transition-all hover:bg-red-600 hover:tracking-wider" 
+            onClick={async () => {
+                setIsLoading(true);
+                await onClick();
+                setIsLoading(false);
+            }}
+        >
+            {isLoading
+                ? <Loader2 className='animate-spin' />
+                : "Clear Tasks"
+            }
+        </button>
+    );
+}
