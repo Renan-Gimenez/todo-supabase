028f714ee0b650e620e40adfd2fca1d4e18e2261
Renan Gimenez <renangiimenez@gmail.com>
2023-10-30 01:24:56 -0300

    fix: unique key

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -3,9 +3,10 @@ import { Skeleton as SkeletonChakra } from '@chakra-ui/react'
 export function Skeleton() {
     return (
         <>
-            {[1, 2, 3].map(() => {
+            {[1, 2, 3].map((index) => {
                 return(
                     <SkeletonChakra
+                        key={index}
                         height='56px'
                         bg='green.500'
                         color='white'
