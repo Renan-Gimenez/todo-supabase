57ffdb55cf9439460121e68cf548dac534dab665
Renan Gimenez <renangiimenez@gmail.com>
2023-11-16 15:37:15 -0300

    fix: fix toggle theme

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,16 +1,29 @@
 import { ThemeContext } from "@/context/ThemeContext";
-import { Moon, Sun } from "lucide-react";
-import { useContext } from "react";
+import { Loader2, Moon, Sun } from "lucide-react";
+import { useContext, useEffect, useState } from "react";
 
 export default function ToggleThemeButton() {
-    const { theme, toggleTheme }:any = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [isClient, setIsClient] = useState(false);
 
-    return(
-        <button className="border border-violet-500/20 p-1 rounded-xl dark:text-white" onClick={toggleTheme}>
-            {theme === 'dark'
-                ? <Sun />
-                : <Moon />
-            }
-        </button>
-    );
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return (
+    <button
+      className="border border-violet-500/20 p-1 rounded-xl dark:text-white"
+      onClick={toggleTheme}
+    >
+      {isClient ? (
+        theme === "dark" ? (
+          <Sun />
+        ) : (
+          <Moon />
+        )
+      ) : (
+        <Loader2 className="animate-spin" />
+      )}
+    </button>
+  );
 }
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,41 +1,46 @@
-"use client"
+"use client";
+import { isBrowser } from "framer-motion";
 import { createContext, useEffect, useState } from "react";
 
 interface ThemeContextType {
-    theme: string;
-    toggleTheme: () => void;
+  theme: string;
+  toggleTheme: () => void;
 }
 
-export const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+export const ThemeContext = createContext<ThemeContextType>(
+  {} as ThemeContextType
+);
 
-export function ThemeProvider({ children }:any) {
-    const inBrowser = typeof window !== "undefined";
+export function ThemeProvider({ children }: any) {
+  const inBrowser = typeof window !== "undefined";
+  const themeInLocalStorage = inBrowser ? localStorage.getItem("theme") : null;
 
-    const themeInLocalStorage = inBrowser ? localStorage.getItem('theme') : null;
-    const deviceTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  const deviceTheme =
+    isBrowser && window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
 
-    const [theme, setTheme] = useState(themeInLocalStorage || deviceTheme);
+  const [theme, setTheme] = useState(themeInLocalStorage || deviceTheme);
 
-    const toggleTheme = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light');
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    console.log({ theme });
+    localStorage.setItem("theme", theme);
+
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      return;
     }
-    
-    useEffect(() => {
-        console.log({theme});
-        localStorage.setItem('theme', theme);
 
-        if (theme === 'dark') {
-            document.documentElement.classList.add('dark');
-            return;    
-        } 
-        
-        document.documentElement.classList.remove('dark');
+    document.documentElement.classList.remove("dark");
+  }, [theme]);
 
-    }, [theme])
-
-    return (
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
