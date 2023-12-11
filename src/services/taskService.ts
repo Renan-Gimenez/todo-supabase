d77ddaf8b6741d4ffa65537fbfd2177430567133
Renan Gimenez <renangiimenez@gmail.com>
2023-12-10 23:28:47 -0300

    feat: order by created_at

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,26 +1,31 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_ANON_KEY || "";
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function addTask(item:TTask){
-    const data = await supabase.from('tasks').insert(item);
-    console.log(data);
+export async function addTask(item: TTask) {
+  const data = await supabase.from("tasks").insert(item);
+  console.log(data);
 }
 
 export async function getTasks(): Promise<TTask[]> {
-    const data = await supabase.from('tasks').select("*");
-    console.log("TASKS:\n", data);    
-    
-    if (!data.data) {
-        return [];
-    }
+  const data = await supabase
+    .from("tasks")
+    .select("*")
+    .order("created_at", { ascending: true });
 
-    return data.data;
+  console.log("TASKS:\n", data);
+
+  if (!data.data) {
+    return [];
+  }
+
+  return data.data;
 }
 
-export async function removeTaskById(id:number) {
-    const data = await supabase.from("tasks").delete().eq("id", id);
-    console.log("REMOVE TASK BY ID:\n", data);
+export async function removeTaskById(id: number) {
+  const data = await supabase.from("tasks").delete().eq("id", id);
+  console.log("REMOVE TASK BY ID:\n", data);
 }
