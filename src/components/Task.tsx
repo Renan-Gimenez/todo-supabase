ded915bb8e9ab8f6bb6ad77ac2259a50a856e091
Renan Gimenez <renangiimenez@gmail.com>
2023-12-18 11:31:27 -0300

    chore: remove ring

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -22,14 +22,14 @@ export default function Task({
     <div className="w-full flex items-center justify-between gap-2 p-3 rounded-lg shadow-lg dark:bg-gray-700">
       <div className="flex items-center gap-2">
         <input
-          className="ring-1 ring-blue-500 form-checkbox m-1 text-indigo-600 self-start rounded-xl h-[16px] w-[16px] flex-1"
+          className="form-checkbox m-1 text-indigo-600 self-start rounded-xl h-[16px] w-[16px] flex-1"
           type="checkbox"
           name="opa"
           checked={done}
           onChange={(e) => toggleTaskDone(id, e.target.checked)}
         />
         <span
-          className={`break-all ring-1 ring-red-500 ${
+          className={`break-all ${
             done ? "text-zinc-500 line-through" : "dark:text-white"
           }`}
         >
