1da69a51bd5b9d88fc9633d0146f93e3f7a0f64e
Renan Gimenez <renangiimenez@gmail.com>
2024-03-08 22:41:46 -0300

    chore: refactor login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ interface SocialLoginButtonType {
 function SocialLoginButton({ icon, children, onClick }: SocialLoginButtonType) {
   return (
     <button
-      className="w-10/12 flex gap-2 px-4 py-2 rounded-full justify-center mx-auto bg-violet-500 text-white"
+      className="w-10/12 flex items-center gap-2 px-4 py-2 rounded-full justify-center mx-auto bg-violet-500 text-white"
       onClick={onClick}
     >
       {icon}
@@ -30,8 +30,7 @@ function SocialLoginButton({ icon, children, onClick }: SocialLoginButtonType) {
 }
 
 export default function LoginPage() {
-  const { user, loading, loginWithGitHub, loginWithTwitter, signOut } =
-    useAuth();
+  const { loading, loginWithGitHub, loginWithTwitter } = useAuth();
 
   if (loading) {
     return (
@@ -44,21 +43,6 @@ export default function LoginPage() {
   return (
     <main className="h-screen w-screen flex items-center justify-center bg-bg-primary dark:text-white p-6">
       <div className="max-w-sm flex flex-col p-8 rounded-2xl shadow-xl bg-zinc-50 dark:bg-gray-800">
-        {/* <div>
-          <section>
-            <strong>LOADING STATUS:</strong>
-            <br />
-            {loading == true ? <span>Loading...</span> : <span>Done</span>}
-          </section>
-
-          {user && (
-            <div className="flex flex-col">
-              <strong>USER INFO:</strong>
-              <span>{user.email}</span>
-              <span>{user.id}</span>
-            </div>
-          )}
-        </div> */}
         <div>
           <div className="w-full flex justify-center gap-4 mb-4">
             <Clipboard className="h-8 w-auto text-violet-500" />
@@ -85,15 +69,7 @@ export default function LoginPage() {
           <SocialLoginButton onClick={loginWithTwitter} icon={<Twitter />}>
             Login with Twitter
           </SocialLoginButton>
-
-          {/* <SocialLoginButton onClick={signOut}>SignOut</SocialLoginButton> */}
         </section>
-
-        {/* <div className="mt-2">
-          <SocialLoginButton onClick={() => console.log(user)}>
-            Log User
-          </SocialLoginButton>
-        </div> */}
       </div>
     </main>
   );
